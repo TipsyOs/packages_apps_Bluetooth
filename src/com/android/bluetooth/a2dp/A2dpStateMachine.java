lt03lte-cafaosp/platform-packages-apps-Bluetooth

A2dp: add a2dp playing state change broadcast during cleanup
A2dp playing state change broadcast lost while bt off. Send A2dp
playing state change broadcast during cleanup.

Change-Id: I0b557a1cbb0052506c07886598a6e00c23f2b900
CRs-Fixed: 1083381

src/com/android/bluetooth/a2dp/A2dpStateMachine.java
@@ -221,6 +221,12 @@ final class A2dpStateMachine extends StateMachine {
         mCleaningup = true;
         for (int i = 0; i < deviceSize; i++) {
              mCurrentDevice = mConnectedDevicesList.get(i);
+             if (mPlayingA2dpDevice.size() != 0 &&
+                     mPlayingA2dpDevice.contains(mCurrentDevice)) {
+                 broadcastAudioState(mCurrentDevice, BluetoothA2dp.STATE_NOT_PLAYING,
+                                     BluetoothA2dp.STATE_PLAYING);
+                 mPlayingA2dpDevice.remove(mCurrentDevice);
+             }
              broadcastConnectionState(mCurrentDevice, BluetoothProfile.STATE_DISCONNECTED,
                                       BluetoothProfile.STATE_CONNECTED);
         }

TipsyOs/packages_apps_Bluetooth

src/com/android/bluetooth/a2dp/A2dpStateMachine.java
@@ -226,6 +226,12 @@ final class A2dpStateMachine extends StateMachine {
         BluetoothDevice device;
         for (int i = 0; i < deviceSize; i++) {
              device = mConnectedDevicesList.get(i);
+             if (mPlayingA2dpDevice.size() != 0 &&
+                     mPlayingA2dpDevice.contains(device)) {
+                 broadcastAudioState(device, BluetoothA2dp.STATE_NOT_PLAYING,
+                                     BluetoothA2dp.STATE_PLAYING);
+                 mPlayingA2dpDevice.remove(device);
+             }
              broadcastConnectionStateImmediate(device, BluetoothProfile.STATE_DISCONNECTED,
                                       BluetoothProfile.STATE_CONNECTED);
         }